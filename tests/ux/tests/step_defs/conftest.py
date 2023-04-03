f64bd35d9367982075ed3ad234b96657184db66b
Arnit Ibrahimi <arnitibra@gmail.com>
2023-04-03 14:13:50 +0100

    added pytest dbb step error to help me debug problems in the future

--- tests/ux/tests/step_defs/conftest.py
+++ tests/ux/tests/step_defs/conftest.py
@@ -17,6 +17,8 @@ from selenium.webdriver.edge.options import (
   )
 # pylint: disable=import-error
 
+def pytest_bdd_step_error(request, feature, scenario, step, step_func, step_func_args, exception):
+    print(f'Step failed: {step}')
 
 def pytest_addoption(parser):
     """Add options for Selenium"""
